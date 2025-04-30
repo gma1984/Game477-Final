4b4b08af856d2569e32bf1f1d8b584ff4f20f282
Cheoddox <leeroywycoff@gmail.com>
2025-04-29 20:27:13 -0500

    Starting on color activation script

---
+++ Assets/Scripts/Player_ActivateColors.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+enum ColorState { None, Red, Green, Blue, Yellow, Cyan, Magenta, White }
+
+public class Player_ActivateColors : MonoBehaviour
+{
+    public GameObject redSignal;
+    public GameObject greenSignal;
+    public GameObject blueSignal;
+    public GameObject yellowSignal;
+    public GameObject cyanSignal;
+    public GameObject magentaSignal;
+    public GameObject whiteSignal;
+    private bool redOn = false;
+    private bool redOn = false;
+    private bool redOn = false;
+    private bool redOn = false;
+    private bool redOn = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && !redOn)
+        {
+            redSignal.SetActive(true);
+            redOn = !redOn;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.R) && redOn)
+        {
+            redSignal.SetActive(false);
+            redOn = !redOn;
+        }
+    }
+}
