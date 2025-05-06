70bcc20a4ed116f85a42f0d6816bc10f008f207d
Matthew Christensen <christensenfamily05@gmail.com>
2025-05-06 17:02:30 -0500

    Moving and Fading Platforms

---
+++ Assets/Scripts/PlatformSlide.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class PlatformSlide : MonoBehaviour
+{
+    public Transform otherPos;
+    public float moveSpeed;
+    [Tooltip("Keep this smaller than 1 for responsive movement")]
+    public float lerpFactor = .9f;
+    public EaseChoice easeChoice = EaseChoice.InOutSine;
+    public bool flip;
+    public bool flippedOnStart;
+
+    public ContactFilter2D touchFilter;
+
+    Vector3 start;
+    Vector3 moveTo;
+    float curDist;
+    float distBetweenPoints;
+
+    private float startScale;
+
+    protected GameObject player;
+
+    float easeInOutSine(float x)
+    {
+        return -(Mathf.Cos(Mathf.PI * x) - 1f) / 2f;
+    }
+
+    float easeInOutQuad(float x)
+    {
+        return x < 0.5f ? 2f * x * x : 1f - Mathf.Pow(-2f * x + 2f, 2f) / 2f;
+    }
+
+    float ease(float x)
+    {
+        switch (easeChoice)
+        {
+            case EaseChoice.InOutSine:
+                return easeInOutSine(x);
+            case EaseChoice.InOutQuad:
+                return easeInOutQuad(x);
+            default:
+                return x;
+        }
+    }
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        if (otherPos == null) otherPos = transform;
+
+        startScale = transform.localScale.x;
+
+        start = transform.position;
+        distBetweenPoints = Vector3.Distance(start, otherPos.position);
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if (moveTo != start) moveTo = otherPos.position;
+
+        curDist = Vector3.Distance(transform.position, moveTo);
+
+        transform.position = Vector3.MoveTowards(transform.position, moveTo, moveSpeed * Time.deltaTime * ease((distBetweenPoints - lerpFactor * curDist) / distBetweenPoints));//Mathf.Lerp(lerpFactor, 1f, curDist/distBetweenPoints));
+
+        if (curDist < .01f) moveTo = moveTo == start ? otherPos.position : start;
+
+        if (flip && moveTo == start)
+        {
+            if (flippedOnStart)
+            {
+                transform.localScale = new Vector2(startScale, transform.localScale.y);
+            }
+            else
+            {
+                transform.localScale = new Vector2(-startScale, transform.localScale.y);
+            }
+        }
+        else
+        {
+            if (flippedOnStart)
+            {
+                transform.localScale = new Vector2(-startScale, transform.localScale.y);
+            }
+            else
+            {
+                transform.localScale = new Vector2(startScale, transform.localScale.y);
+            }
+        }
+    }
+
+    protected virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            bool onPlatform = true;//collision.gameObject.GetComponent<PlayerPhysics>().IsGroundedTo(GetComponentInChildren<Collider2D>());
+
+            if (onPlatform)
+            {
+                collision.gameObject.transform.SetParent(transform, true);
+                player = collision.gameObject;
+            }
+        }
+    }
+
+    protected virtual void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.SetParent(null);
+            player = null;
+        }
+    }
+}
+
+public enum EaseChoice
+{
+    InOutSine,
+    InOutQuad
+}
---
+++ Assets/Scripts/PlatformSlideFading.cs
@@ -0,0 +1,142 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSlideFading : PlatformSlide
+{
+    public bool fadeOverTime;
+    public float holdFade = 1f;
+    public float fadeAmount = 0f;
+    public float fadeSpeed = 1f;
+
+    public bool mouseDebug = false;
+
+    //bool wasFading = false;
+    bool fading = false;
+    float fadeEnd;
+
+    Collider2D col;
+
+    SpriteRenderer sprite;
+    Color tmp;
+
+    private void OnFaded()
+    {
+        //col.isTrigger = true;
+        col.enabled = false;
+        if (player)
+        {
+            player.transform.SetParent(null);
+            player = null;
+        }
+    }
+
+    private void OnUnfadeStart()
+    {
+
+    }
+
+    private void OnUnfadeEnd()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+
+        sprite = GetComponentInChildren<SpriteRenderer>();
+        col = GetComponentInChildren<Collider2D>();
+    }
+
+    private void StartFading()
+    {
+        if (fadeAmount == 1f) return;
+
+        fadeAmount = Mathf.MoveTowards(fadeAmount, 1f, fadeSpeed * Time.fixedDeltaTime);
+        fading = true;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+
+        if (!fadeOverTime)
+        {
+            if (fading)
+            {
+                fadeAmount = Mathf.MoveTowards(fadeAmount, 1f, fadeSpeed * Time.deltaTime);
+
+                if (fadeAmount == 1f)
+                {
+                    fading = false;
+                    fadeEnd = Time.time + holdFade;
+                    OnFaded();
+                }
+            }
+            else if (Time.time > fadeEnd)
+            {
+                fadeAmount = Mathf.MoveTowards(fadeAmount, 0f, fadeSpeed * Time.deltaTime);
+                col.enabled = true;
+                //col.isTrigger = false;
+            }
+        }
+        else
+        {
+            if (fading && fadeAmount == 1f)
+            {
+                fading = false;
+                fadeEnd = Time.time + holdFade;
+                OnFaded();
+            }
+
+            if (fading == false && (fadeAmount != 1f || Time.time > fadeEnd))
+            {
+                fadeAmount = Mathf.MoveTowards(fadeAmount, 0f, fadeSpeed * Time.deltaTime);
+                //col.isTrigger = false;
+                col.enabled = true;
+            }
+
+            fading = false;
+        }
+
+        tmp = sprite.color;
+        tmp.a = 1f - fadeAmount;
+        sprite.color = tmp;
+    }
+
+    protected override void OnCollisionEnter2D(Collision2D collision)
+    {
+        base.OnCollisionEnter2D(collision);
+        if (!fadeOverTime && !fading && collision.gameObject.tag == "Player")
+        {
+            fading = true;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Player") return;
+
+        if (fadeOverTime)
+        {
+            StartFading();
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (!mouseDebug) return;
+
+
+
+        if (fadeOverTime)
+        {
+            StartFading();
+        }
+
+        fading = true;
+    }
+}
